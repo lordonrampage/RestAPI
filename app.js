7bfdb1ae3d237bbeb5083444a7ba4be489fb8374
lordonrampage <creative.lordonrampage.com>
2020-08-09 00:51:16 +0530

    added the code for api

--- app.js
+++ app.js
@@ -16,7 +16,55 @@ app.use(
 );
 app.use(express.static("public"));
 
-//TODO
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
+});
+
+const articleSchema = {
+  title: String,
+  content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+/////////////////////////All Articles///////////////////////////////////
+
+app
+  .route("/articles")
+  .get(function (req, res) {
+    Article.find(function (err, foundArticles) {
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .post(function (req, res) {
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
+
+    newArticle.save(function (err) {
+      if (!err) {
+        res.send("successfully sent the new Article");
+      } else {
+        res.send(err);
+      }
+    });
+  })
+  .delete(function (req, res) {
+    Article.deleteMany(function (err) {
+      if (!err) {
+        res.send("successfully deleted");
+      } else {
+        res.send(err);
+      }
+    });
+  });
+
+/////////////////////////Individual Articles///////////////////////////////////
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
